66fd791b86f9fc9fa05c0787de57975e53143261
Matheus Balcky <matheuscabal821@gmail.com>
2022-09-27 23:11:10 -0300

    FEAT: Middleware to authenticate token

---
+++ src/middlewares/tokenAuthenticationMiddle.ts
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import { NextFunction, Request, Response } from 'express';
+import * as jwt from '../utils/jwtUtils';
+
+export async function tokenAuthenticationMiddle(req: Request, res: Response, next: NextFunction) {
+  const error = { type: 'unauthorized', message: 'Unauthorized token!' };
+  const authorization = req.headers['authorization'];
+  if (!authorization) throw error;
+
+  const token = authorization.replace('Bearer ', '');
+  if (!token) throw error;
+
+  const result = jwt.verifyToken(token);
+  if (!result) throw error;
+
+  next();
+}

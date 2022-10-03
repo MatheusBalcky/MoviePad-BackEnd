7ff176221d3b72c5cf32b53b08a2e119fb0a538c
Matheus Balcky <matheuscabal821@gmail.com>
2022-10-03 16:24:19 -0300

    RENAME SCHEMA

--- src/schemas/authSchemas.ts
+++ src/schemas/authSchemas.ts
@@ -3,7 +3,7 @@ import Joi from 'joi';
 export const signUp = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(8).required(),
-  repeatPassword: Joi.ref('password')
+  passwordConfirm: Joi.ref('password')
 });
 
 export const signIn = Joi.object({

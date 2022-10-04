4908ab25e591bfe9d360db0cd250fd05b7d4e8d4
Matheus Balcky <matheuscabal821@gmail.com>
2022-10-03 21:51:48 -0300

    FEAT: tokenVerify controller added to authenticate token

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import * as authService from '../services/authServices';
 import * as interfaces from '../interfaces/interfaces';
+import prisma from '../database/database';
 
 export async function signUp(req: Request, res: Response) {
   const { email, password } = req.body;
@@ -14,7 +15,18 @@ export async function signUp(req: Request, res: Response) {
 export async function signIn(req: Request, res: Response) {
   const userData: interfaces.userData = req.body;
 
-  const token = await authService.signIn(userData);
+  const userAndToken = await authService.signIn(userData);
 
-  res.status(200).send({ token });
+  res.status(200).send(userAndToken);
+}
+
+export async function tokenVerify(req: Request, res: Response) {
+  const { userId } = res.locals.tokenData;
+  const userData = await prisma.users.findUnique({ where: { id: userId } });
+  const user = {
+    id: userData?.id,
+    email: userData?.email,
+    createdAt: userData?.createdAt
+  };
+  res.status(200).send(user);
 }

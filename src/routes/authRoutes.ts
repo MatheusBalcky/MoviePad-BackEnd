0d05de7a071154c626851f2f0385a2df0b53fc44
Matheus Balcky <matheuscabal821@gmail.com>
2022-10-03 21:52:16 -0300

    FEAT: Route verifyToken added

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,10 +2,13 @@ import { Router } from 'express';
 import { validateSchemaMiddleware } from '../middlewares/schemaMiddleware';
 import * as authControllers from '../controllers/authController';
 import * as authSchemas from '../schemas/authSchemas';
+import { tokenAuthenticationMiddle } from '../middlewares/tokenAuthenticationMiddle';
 
 const authRoutes = Router();
 
 authRoutes.post('/signup', validateSchemaMiddleware(authSchemas.signUp), authControllers.signUp);
 authRoutes.post('/signin', validateSchemaMiddleware(authSchemas.signIn), authControllers.signIn);
+
+authRoutes.post('/verifyToken', tokenAuthenticationMiddle, authControllers.tokenVerify);
 
 export default authRoutes;

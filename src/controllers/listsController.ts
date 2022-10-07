6698508c97ee9d85e561bcbab2f188848cdbb879
Matheus Balcky <matheuscabal821@gmail.com>
2022-10-07 13:56:03 -0300

    👌 IMPROVE:

--- src/controllers/listsController.ts
+++ src/controllers/listsController.ts
@@ -4,13 +4,12 @@ import * as ListsServices from '../services/listsService'
 
 export async function createList(req: Request, res: Response) {
   const { userId } = res.locals.tokenData;
-
   const listData: Interfs.ListData = { userId, ...req.body };
+
   await ListsServices.insertList(listData);
 
   res.status(201).send(listData);
 }
-
 
 export async function getListsFromUser(req: Request, res: Response) {
   const { userId } = res.locals.tokenData;

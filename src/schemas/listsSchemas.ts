e048cf7c2777156a931634bbf6b7c2727ea03f12
Matheus Balcky <matheuscabal821@gmail.com>
2022-10-08 15:51:22 -0300

    📦 NEW: Schema to validate content data

--- src/schemas/listsSchemas.ts
+++ src/schemas/listsSchemas.ts
@@ -4,3 +4,15 @@ export const newList = Joi.object({
   title: Joi.string().max(45).required(),
   iconList: Joi.string().min(0)
 });
+
+export const contentData = Joi.object({
+  contentId: Joi.number().required(),
+  title: Joi.string().required(),
+  pictureUrl: Joi.string()
+    .pattern(/^https?:\/\/.*\/.*\.(png|gif|webp|jpeg|jpg)\??.*$/)
+    .required(),
+  description: Joi.string().min(0).required(),
+  releaseYear: Joi.date().required(),
+  trailerUrl: Joi.string().min(0).required(),
+  rating: Joi.number()
+});

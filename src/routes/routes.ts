66a8c6c56ca8888a60d69ff94e1a62551e97881f
Matheus Balcky <matheuscabal821@gmail.com>
2022-10-10 17:44:09 -0300

    unecessary route removed

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,8 @@
-import { Request, Response,Router } from 'express';
+import { Router } from 'express';
 import authRoutes from './authRoutes';
 import listsRoutes from './listsRoutes';
 
 const routes = Router();
-
-routes.get('/testmoviepad', (req: Request, res: Response) => {
-  res.status(200).send('Hellow World');
-});
 
 routes.use(authRoutes);
 routes.use(listsRoutes);

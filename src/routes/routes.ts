87d57bf48d5f2bc237f241b4004216ed2e65f4a1
Matheus Balcky <matheuscabal821@gmail.com>
2022-10-10 15:07:00 -0300

    BUILD: Docker added, tsconfig outdir added, new route to test app

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,12 @@
-import { Router } from 'express';
+import { Request, Response,Router } from 'express';
 import authRoutes from './authRoutes';
 import listsRoutes from './listsRoutes';
 
 const routes = Router();
+
+routes.get('/testmoviepad', (req: Request, res: Response) => {
+  res.status(200).send('Hellow World');
+});
 
 routes.use(authRoutes);
 routes.use(listsRoutes);
